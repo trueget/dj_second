9a2520354c531568980e16f68706301223cd9018
roma <trueget33@gmail.com>
2022-06-21 22:16:38 +0300

    21.06.2022 22:16

--- something/something/urls.py
+++ something/something/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.first_page),
-    path('thanks/', views.t_page, name='t_page'),
+    path('thanks/', views.thanks_page, name='thanks_page'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
